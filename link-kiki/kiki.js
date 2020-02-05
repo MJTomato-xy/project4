5c6aa4a509c86b45ce14daffe4dafb51eaf622fb
Xinyi-Liu-024 <59549169+Xinyi-Liu-024@users.noreply.github.com>
2020-02-05 17:08:08 -0500

    links

---
+++ link-kiki/kiki.js
@@ -0,0 +1,9 @@
+function scrollDown() {
+    var elmt = document.querySelector(".page1")
+    elmt.scrollBy(0, 1500);
+  }
+
+  function closeBar() {
+      var elmt = document.querySelector(".page1")
+      elmt.scrollBy(0, -1500);
+  }
